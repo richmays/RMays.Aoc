eeee813d84d9b4c4bbd993116b014c1ba4f3d9bb
Rich Mays <richmays+github@gmail.com>
2019-12-04 00:36:43 -0500

    D4.  Embarassing.  Tried to take a shortcut, and failed miserably.  I should've done slow-and-steady; probably could've halved my time.

---
+++ RMays.Aoc2019.Tests/Day4Tests.cs
@@ -0,0 +1,87 @@
+﻿using NUnit.Framework;
+using RMays.Aoc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMays.Aoc2019.Tests
+{
+    [TestFixture]
+    public class Day4Tests
+    {
+        private string inputData = InputData.Day4;
+        private string knownOutputA = "123";
+        private string knownOutputB = "456";
+
+        private IDay<long> GetDayObject()
+        {
+            return new Day4();
+        }
+
+        [Test]
+        [TestCase("111111", 1)]
+        [TestCase("223450", 0)]
+        [TestCase("123789", 0)]
+        public void PartATests(string input, int expectedOutput)
+        {
+            var day = GetDayObject();
+            var result = day.SolveA(input);
+            Assert.AreEqual(expectedOutput, result);
+        }
+
+        [Test]
+        [TestCase("4, 5, 6", 456)]
+
+        public void PartBTests(string input, int expectedOutput)
+        {
+            var day = GetDayObject();
+            var result = day.SolveB(input);
+            Assert.AreEqual(expectedOutput, result);
+        }
+
+        [Test]
+        public void DoItA() // ?
+        {
+            var day = GetDayObject();
+            Console.WriteLine(day.SolveA(inputData));
+        }
+
+        [Test]
+        public void DoItB() // ?
+        {
+            var day = GetDayObject();
+            Console.WriteLine(day.SolveB(inputData));
+        }
+
+        [Test]
+        public void DoItA_Answer()
+        {
+            var day = GetDayObject();
+            var result = day.SolveA(inputData);
+            Assert.AreEqual(knownOutputA, result.ToString());
+        }
+
+        [Test]
+        [TestCase(112233, true)]
+        [TestCase(123444, false)]
+        [TestCase(111122, true)]
+        [TestCase(111222, false)]
+        [TestCase(111234, false)]
+        [TestCase(111123, false)]
+        public void MatchesTests(int numToTest, bool expectedResult)
+        {
+            var day = new Day4();
+            Assert.AreEqual(expectedResult, day.Matches(numToTest));
+        }
+
+        [Test]
+        public void DoItB_Answer()
+        {
+            var day = GetDayObject();
+            var result = day.SolveB(inputData);
+            Assert.AreEqual(knownOutputB, result.ToString());
+        }
+    }
+}
---
+++ RMays.Aoc2019/Day4.cs
@@ -0,0 +1,137 @@
+﻿using RMays.Aoc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMays.Aoc2019
+{
+    public class Day4 : IDay<long>
+    {
+        public long SolveA(string input)
+        {
+            return RealSolveA(248345, 746315);
+
+            int thecount = 0;
+            //var myList = Parser.Tokenize(input, '-');
+            for(int a = 2; a <= 7; a++)
+            {
+                for (int b = a; b <= 9; b++)
+                {
+                    if (a == 2 && b < 4) continue;
+                    for(int c = b; c <= 9; c++)
+                    {
+                        if (a == 2 && b == 4 && c < 8) continue;
+                        for(int d = c; d <= 9; d++)
+                        {
+                            if (a == 2 && b == 4 && c == 8 && d < 3) continue;
+                            for (int e = d; e <= 9; e++)
+                            {
+                                if (a == 2 && b == 4 && c == 8 && d == 3 && e < 4) continue;
+                                for (int f = e; f <= 9; f++)
+                                {
+                                    if (a == 2 && b == 4 && c == 8 && d == 3 && e == 4 && f < 5) continue;
+                                    
+                                    // Adjacent
+                                    if (a != b && b != c && c != d && d != e && e != f) continue;
+
+                                    thecount++;
+
+                                    if (a == 7 && b == 4 && c == 6 && d == 3 && e == 1 && f == 5) return thecount;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return thecount;
+        }
+
+        private int RealSolveA(int start, int end)
+        {
+            // 1019
+            int theCount = 0;
+            for(int i = start; i <= end; i++)
+            {
+                var s = i.ToString();
+                bool foundNonSequential = false;
+                bool foundMatch = false;
+                for(int x = 0; x <= 4; x++)
+                {
+                    if (s[x] == s[x + 1]) foundMatch = true;
+                    if (s[x] > s[x + 1]) foundNonSequential = true;
+                }
+
+                if (foundMatch && !foundNonSequential) theCount++;
+                
+            }
+            return theCount;
+        }
+
+        public long SolveB(string input)
+        {
+            return RealSolveB(248345, 746315);
+        }
+
+        private int RealSolveB(int start, int end)
+        {
+            // ?
+            int theCount = 0;
+            var matches = new List<char>();
+
+            for (int i = start; i <= end; i++)
+            {
+                theCount += Matches(i) ? 1 : 0;
+            }
+            return theCount;
+        }
+
+        public bool Matches(int i)
+        {
+            var s = i.ToString();
+
+            // Quick sequential check.
+            var foundNonSequential = false;
+            for (int x = 0; x <= 4; x++)
+            {
+                if (s[x] > s[x + 1]) foundNonSequential = true;
+            }
+            if (foundNonSequential) return false;
+
+            var matches = new List<char>();
+            bool foundMatch = false;
+            for (int x = 0; x <= 4; x++)
+            {
+                if (s[x] == s[x + 1])
+                {
+                    foundMatch = true;
+                }
+            }
+
+            if (!foundMatch) return false;
+
+            // Now do the Dict check on each digit.
+            var dict = new Dictionary<char, int>();
+            for(int x = 0; x < 6; x++)
+            {
+                if (dict.ContainsKey(s[x]))
+                {
+                    dict[s[x]]++;
+                }
+                else
+                { 
+                    dict.Add(s[x], 1);
+                }
+            }
+
+            foreach(var item in dict.Values)
+            {
+                if (item == 2) return true;
+            }
+
+            return false;
+        }
+    }
+}
