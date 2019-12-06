b8fa65d00da01ca90abc3bc5ff1651793c094c4e
Rich Mays <richmays+github@gmail.com>
2019-12-06 00:36:38 -0500

    D6 done.  Started 11 minutes late; finished first part in 14 minutes, both in 24 minutes.  not bad!

---
+++ RMays.Aoc2019.Tests/Day6Tests.cs
@@ -0,0 +1,93 @@
+﻿using NUnit.Framework;
+using RMays.Aoc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMays.Aoc2019.Tests
+{
+    [TestFixture]
+    public class Day6Tests
+    {
+        private string inputData = InputData.Day6;
+        private string knownOutputA = "402879";
+        private string knownOutputB = "456";
+
+        private IDay<long> GetDayObject()
+        {
+            return new Day6();
+        }
+
+        [Test]
+        [TestCase(@"COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L", 42)]
+        public void PartATests(string input, int expectedOutput)
+        {
+            var day = GetDayObject();
+            var result = day.SolveA(input);
+            Assert.AreEqual(expectedOutput, result);
+        }
+
+        [Test]
+        [TestCase(@"COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L
+K)YOU
+I)SAN", 4)]
+        public void PartBTests(string input, int expectedOutput)
+        {
+            var day = GetDayObject();
+            var result = day.SolveB(input);
+            Assert.AreEqual(expectedOutput, result);
+        }
+
+        [Test]
+        public void DoItA() // ?
+        {
+            var day = GetDayObject();
+            Console.WriteLine(day.SolveA(inputData));
+        }
+
+        [Test]
+        public void DoItB() // ?
+        {
+            var day = GetDayObject();
+            Console.WriteLine(day.SolveB(inputData));
+        }
+
+        [Test]
+        public void DoItA_Answer()
+        {
+            var day = GetDayObject();
+            var result = day.SolveA(inputData);
+            Assert.AreEqual(knownOutputA, result.ToString());
+        }
+
+        [Test]
+        public void DoItB_Answer()
+        {
+            var day = GetDayObject();
+            var result = day.SolveB(inputData);
+            Assert.AreEqual(knownOutputB, result.ToString());
+        }
+    }
+}
