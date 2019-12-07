f71563e99f179e350afea982d17547c68c6e9e3b
Rich Mays <richmays+github@gmail.com>
2019-12-07 02:13:34 -0500

    D7 done

---
+++ RMays.Aoc2019/Day7.cs
@@ -0,0 +1,151 @@
+﻿using RMays.Aoc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMays.Aoc2019
+{
+    public class Day7 : IDay<long>
+    {
+        public Day7() { }
+
+        public long SolveA(string input)
+        {
+            long maxValue = 0;
+            for (int a = 0; a < 5; a++)
+            {
+                for (int b = 0; b < 5; b++)
+                {
+                    if (a == b) continue;
+                    for (int c = 0; c < 5; c++)
+                    {
+                        if (a == c || b == c) continue;
+                        for (int d = 0; d < 5; d++)
+                        {
+                            if (a == d || b == d || c == d) continue;
+                            for (int e = 0; e < 5; e++)
+                            {
+                                if (a == e || b == e || c == e || d == e) continue;
+                                var comp1 = new IntcodeComp(input);
+                                comp1.Initialize();
+                                var output1 = comp1.Run(new List<long>() { a, 0 });
+                                comp1.Initialize();
+                                var output2 = comp1.Run(new List<long>() { b, output1 });
+                                comp1.Initialize();
+                                var output3 = comp1.Run(new List<long>() { c, output2 });
+                                comp1.Initialize();
+                                var output4 = comp1.Run(new List<long>() { d, output3 });
+                                comp1.Initialize();
+                                var output5 = comp1.Run(new List<long>() { e, output4 });
+                                if (output5 > maxValue) maxValue = output5;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return maxValue;
+        }
+
+        public long SolveB(string program)
+        {
+            long maxValue = 0;
+            for (int a = 0; a < 5; a++)
+            {
+                for (int b = 0; b < 5; b++)
+                {
+                    if (a == b) continue;
+                    for (int c = 0; c < 5; c++)
+                    {
+                        if (a == c || b == c) continue;
+                        for (int d = 0; d < 5; d++)
+                        {
+                            if (a == d || b == d || c == d) continue;
+                            for (int e = 0; e < 5; e++)
+                            {
+                                if (a == e || b == e || c == e || d == e) continue;
+
+                                var AmpIds = new List<int> { a, b, c, d, e };
+                                var result = GetCountForSignals(program, AmpIds);
+                                /*
+                                var comp1 = new Day7();
+                                var output1 = comp1.Solve(input, new List<int>() { a, 0 });
+                                var output2 = comp1.Solve(input, new List<int>() { b, (int)output1 });
+                                var output3 = comp1.Solve(input, new List<int>() { c, (int)output2 });
+                                var output4 = comp1.Solve(input, new List<int>() { d, (int)output3 });
+                                var output5 = comp1.Solve(input, new List<int>() { e, (int)output4 });
+                                */
+                                if (result > maxValue) maxValue = result;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return maxValue;
+        }
+
+        private long GetCountForSignals(string program, List<int> AmpIds)
+        {
+            var amp1 = new IntcodeComp(program, $"Amp1 ({AmpIds[0] + 5})");
+            amp1.Initialize();
+            var amp2 = new IntcodeComp(program, $"Amp2 ({AmpIds[1] + 5})");
+            amp2.Initialize();
+            var amp3 = new IntcodeComp(program, $"Amp3 ({AmpIds[2] + 5})");
+            amp3.Initialize();
+            var amp4 = new IntcodeComp(program, $"Amp4 ({AmpIds[3] + 5})");
+            amp4.Initialize();
+            var amp5 = new IntcodeComp(program, $"Amp5 ({AmpIds[4] + 5})");
+            amp5.Initialize();
+
+            amp1.InjectInput(AmpIds[0] + 5);
+            amp2.InjectInput(AmpIds[1] + 5);
+            amp3.InjectInput(AmpIds[2] + 5);
+            amp4.InjectInput(AmpIds[3] + 5);
+            amp5.InjectInput(AmpIds[4] + 5);
+
+            amp1.InjectInput(0);
+
+            int breakout = 0;
+            while (true)
+            {
+                var output1 = amp1.Run();
+
+                amp2.InjectInput(output1);
+                var output2 = amp2.Run();
+
+                amp3.InjectInput(output2);
+                var output3 = amp3.Run();
+
+                amp4.InjectInput(output3);
+                var output4 = amp4.Run();
+
+                amp5.InjectInput(output4);
+                var output5 = amp5.Run();
+
+                if (amp5.IsHalted)
+                {
+                    return output5;
+                }
+                else
+                {
+                    /*
+                    amp1.InjectInput(AmpIds[0] + 5);
+                    amp2.InjectInput(AmpIds[1] + 5);
+                    amp3.InjectInput(AmpIds[2] + 5);
+                    amp4.InjectInput(AmpIds[3] + 5);
+                    amp5.InjectInput(AmpIds[4] + 5);
+                    */
+                    amp1.InjectInput(output5);
+                }
+
+                breakout++;
+            }
+
+            // bad times!
+            return -1;
+        }
+    }
+}
---
+++ RMays.Aoc2019/IntcodeComp.cs
@@ -0,0 +1,299 @@
+﻿using RMays.Aoc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMays.Aoc2019
+{
+    public class IntcodeComp
+    {
+        public string Program { get; set; }
+
+        public IntcodeComp() { }
+
+        public IntcodeComp(string program, string ampName = "")
+        {
+            this.Program = program;
+            this.AmpName = ampName;
+        }
+
+        private string AmpName;
+        private List<long> ProgramInput = new List<long>();
+        private int CurrInputId = 0;
+        private int CurrInstructionId = 0;
+        private List<int> list; // the program that's running now; includes changed values.
+
+        public void Initialize()
+        {
+            var myList = Parser.Tokenize(this.Program);
+            list = new List<int>();
+            foreach (var item in myList)
+            {
+                list.Add(int.Parse(item));
+            }
+
+            this.IsHalted = false;
+            this.IsWaitingForInput = false;
+            this.CurrInputId = 0;
+            this.CurrInstructionId = 0;
+        }
+
+        public long Run(List<long> NewInputVals)
+        {
+            this.CurrInputId = 0;
+            ProgramInput = new List<long>();
+            foreach(var inputval in NewInputVals)
+            {
+                InjectInput(inputval);
+            }
+            return Run(0);
+        }
+
+        public long Run()
+        {
+            return Run(CurrInstructionId);
+        }
+
+        public long Run(int startingInstructionId = 0)
+        {
+            int currId = startingInstructionId;
+            int outputVal = 0;
+            while (currId >= 0 && currId < list.Count() && !IsHalted)
+            {
+                int firstVal;
+                int secondVal;
+                int valToStore = -1;
+
+                string instruction = list[currId].ToString("00000");
+                string OpCode = instruction.Substring(3, 2);
+                var Param1Pos = (instruction[2] == '0');
+                var Param2Pos = (instruction[1] == '0');
+                var Param3Pos = (instruction[0] == '0');
+
+                // 1 is Immediate mode, 0 is Position mode.  the first computer was all Position mode.
+
+                switch (OpCode)
+                {
+                    case "01": // add (3)
+                        firstVal = (Param1Pos ? list[list[currId + 1]] : list[currId + 1]);
+                        secondVal = (Param2Pos ? list[list[currId + 2]] : list[currId + 2]);
+                        if (Param3Pos)
+                        {
+                            list[list[currId + 3]] = firstVal + secondVal;
+                            PrintLinePrefix(currId, 3);
+                            PrintDebug($"Add {firstVal} and {secondVal}, store in position {list[currId + 3]}.");
+                        }
+                        else
+                        {
+                            list[currId + 3] = firstVal + secondVal;
+                            PrintLinePrefix(currId, 3);
+                            PrintDebug($"Add {firstVal} and {secondVal}, store in position {currId + 3}.");
+                        }
+                        currId += 4;
+                        break;
+                    case "02": // mult (3)
+                        firstVal = (Param1Pos ? list[list[currId + 1]] : list[currId + 1]);
+                        secondVal = (Param2Pos ? list[list[currId + 2]] : list[currId + 2]);
+                        if (Param3Pos)
+                        {
+                            list[list[currId + 3]] = firstVal * secondVal;
+                            PrintLinePrefix(currId, 3);
+                            PrintDebug($"Multiply {firstVal} and {secondVal}, store in position {list[currId + 3]}.");
+                        }
+                        else
+                        {
+                            list[currId + 3] = firstVal * secondVal;
+                            PrintLinePrefix(currId, 3);
+                            PrintDebug($"Multiply {firstVal} and {secondVal}, store in position {currId + 3}.");
+                        }
+                        currId += 4;
+                        break;
+                    case "03": // read input (1)
+                        if (CurrInputId >= ProgramInput.Count())
+                        {
+                            this.IsWaitingForInput = true;
+                            PrintWarn($"{this.AmpName} is waiting for input.");
+                            this.CurrInstructionId = currId;
+                            return outputVal;
+                        }
+
+                        if (Param1Pos)
+                        {
+                            list[list[currId + 1]] = (int)ProgramInput[CurrInputId];
+                            CurrInputId++;
+                            PrintLinePrefix(currId, 1);
+                            PrintDebug($"Read input ({list[list[currId + 1]]}), store in position {list[currId + 1]}.");
+                        }
+                        else
+                        {
+                            list[currId + 1] = (int)ProgramInput[CurrInputId];
+                            CurrInputId++;
+                            PrintLinePrefix(currId, 1);
+                            PrintDebug($"Read input ({list[list[currId + 1]]}), store in position {currId + 1}.");
+                        }
+                        currId += 2;
+                        break;
+                    case "04": // write output (1)
+                        if (Param1Pos)
+                        {
+                            outputVal = list[list[currId + 1]];
+                            PrintLinePrefix(currId, 1);
+                            PrintDebug($"Write value of position {list[currId + 1]} ({outputVal}).");
+                        }
+                        else
+                        {
+                            outputVal = list[currId + 1];
+                            PrintLinePrefix(currId, 1);
+                            PrintDebug($"Write value of position {currId + 1} ({outputVal}).");
+                        }
+                        PrintWarn($"OUTPUT ({this.AmpName}): " + outputVal);
+                        currId += 2;
+                        break;
+                    case "05": // jump if true (2)
+                        firstVal = (Param1Pos ? list[list[currId + 1]] : list[currId + 1]);
+                        secondVal = (Param2Pos ? list[list[currId + 2]] : list[currId + 2]);
+                        if (firstVal != 0)
+                        {
+                            PrintLinePrefix(currId, 2);
+                            PrintDebug($"Value {firstVal} is true (non-0).  Jump to instruction {secondVal}.");
+                            currId = secondVal;
+                        }
+                        else
+                        {
+                            PrintLinePrefix(currId, 2);
+                            PrintDebug($"Value {firstVal} is false (0).  Continue.");
+                            currId += 3;
+                        }
+                        break;
+                    case "06": // jump if false (2)
+                        firstVal = (Param1Pos ? list[list[currId + 1]] : list[currId + 1]);
+                        secondVal = (Param2Pos ? list[list[currId + 2]] : list[currId + 2]);
+                        if (firstVal == 0)
+                        {
+                            PrintLinePrefix(currId, 2);
+                            PrintDebug($"Value {firstVal} is false (0).  Jump to instruction {secondVal}.");
+                            currId = secondVal;
+                        }
+                        else
+                        {
+                            PrintLinePrefix(currId, 2);
+                            PrintDebug($"Value {firstVal} is true.  Continue.");
+                            currId += 3;
+                        }
+                        break;
+                    case "07": // less than (3)
+                        firstVal = (Param1Pos ? list[list[currId + 1]] : list[currId + 1]);
+                        secondVal = (Param2Pos ? list[list[currId + 2]] : list[currId + 2]);
+
+                        if (firstVal < secondVal)
+                        {
+                            valToStore = 1;
+                        }
+                        else
+                        {
+                            valToStore = 0;
+                        }
+
+                        if (Param3Pos)
+                        {
+                            list[list[currId + 3]] = valToStore;
+                            PrintLinePrefix(currId, 2);
+                            PrintDebug($"Value {firstVal} is {(valToStore == 0 ? "NOT " : "")}less than {secondVal}.  Write {valToStore} to position {list[currId + 3]}.");
+                        }
+                        else
+                        {
+                            list[currId + 3] = valToStore;
+                            PrintLinePrefix(currId, 2);
+                            PrintDebug($"Value {firstVal} is {(valToStore == 0 ? "NOT " : "")}less than {secondVal}.  Write {valToStore} to position {currId + 3}.");
+                        }
+
+                        currId += 4;
+                        break;
+                    case "08": // equals (3)
+                        firstVal = (Param1Pos ? list[list[currId + 1]] : list[currId + 1]);
+                        secondVal = (Param2Pos ? list[list[currId + 2]] : list[currId + 2]);
+
+                        if (firstVal == secondVal)
+                        {
+                            valToStore = 1;
+                        }
+                        else
+                        {
+                            valToStore = 0;
+                        }
+
+                        if (Param3Pos)
+                        {
+                            list[list[currId + 3]] = valToStore;
+                            PrintLinePrefix(currId, 3);
+                            PrintDebug($"Value {firstVal} is {(valToStore == 0 ? "NOT " : "")}equal to {secondVal}.  Write {valToStore} to position {list[currId + 3]}.");
+                        }
+                        else
+                        {
+                            list[currId + 3] = valToStore;
+                            PrintLinePrefix(currId, 3);
+                            PrintDebug($"Value {firstVal} is {(valToStore == 0 ? "NOT " : "")}equal to {secondVal}.  Write {valToStore} to position {currId + 3}.");
+                        }
+
+                        currId += 4;
+                        break;
+                    case "99": // halt
+                        PrintDebug("HALT; end (99)");
+                        PrintLinePrefix(currId, 0);
+                        PrintDebug($"Halt!");
+                        IsHalted = true;
+                        break;
+                    default:
+                        PrintDebug("HALT; bad instruction: " + list[currId]);
+                        return -1;
+                }
+            }
+
+            return outputVal;
+        }
+
+        public bool IsHalted { get; set; } = false;
+        public bool IsWaitingForInput { get; set; } = false;
+       
+        public long Solve(string program, List<long> inputVals)
+        {
+            this.Program = program;
+            this.Initialize();
+            return Run(inputVals);
+        }
+
+        private void PrintDebug(string debugLine)
+        {
+            //Console.WriteLine(debugLine);
+        }
+
+        private void PrintWarn(string debugLine)
+        {
+            //Console.WriteLine(debugLine);
+        }
+
+        private void PrintLinePrefix(int instructionId, int paramsToPrint)
+        {
+            /*
+            Console.Write($"[{instructionId}");
+            for (int i = 1; i <= paramsToPrint; i++)
+            {
+                Console.Write($",{instructionId + i}");
+            }
+            Console.Write($"] ");
+            */
+        }
+
+        public void InjectInput(long newInput)
+        {
+            ProgramInput.Add(newInput);
+            PrintWarn($"Injecting Input ({this.AmpName}): {newInput}");
+            IsWaitingForInput = false;
+        }
+
+        
+    }
+}
+
