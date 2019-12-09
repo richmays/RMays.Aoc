2be69f838a5d7f252b5949d94715e7e79fb01a56
Rich Mays <richmays+github@gmail.com>
2019-12-09 01:47:35 -0500

    D9 done.  Tests for days 2 and 5 still pass, even with my overhaul of the Intcode computer.

---
+++ RMays.Aoc2019/Day9.cs
@@ -0,0 +1,38 @@
+﻿using RMays.Aoc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMays.Aoc2019
+{
+    public class Day9 : IDay<long>
+    {
+        public long SolveA(string input)
+        {
+            var Compy = new IntcodeComp(input);
+            Compy.Initialize();
+            Compy.InjectInput(1);
+            var results = Compy.Run9();
+            return results[0];
+        }
+
+        public List<long> Solve(string input)
+        {
+            var Compy = new IntcodeComp(input);
+            Compy.Initialize();
+            var result = Compy.Run9();
+            return Compy.Outputs;
+        }
+
+        public long SolveB(string input)
+        {
+            var Compy = new IntcodeComp(input);
+            Compy.Initialize();
+            Compy.InjectInput(2);
+            var results = Compy.Run9();
+            return results[0];
+        }
+    }
+}
