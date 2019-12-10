f43ea220de351ad548d2356e6d4863f4c69233e5
Rich Mays <richmays+github@gmail.com>
2019-12-10 01:36:33 -0500

    D10 part 1 done.  i transposed 'r' and 'c' throughout.  oops.

---
+++ RMays.Aoc2019.Tests/Day10Tests.cs
@@ -0,0 +1,127 @@
+﻿using NUnit.Framework;
+using RMays.Aoc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMays.Aoc2019.Tests
+{
+    [TestFixture]
+    public class Day10Tests
+    {
+        private string inputData = InputData.Day10;
+        private string knownOutputA = "123";
+        private string knownOutputB = "456";
+
+        private IDay<long> GetDayObject()
+        {
+            return new Day10();
+        }
+
+        [Test]
+        [TestCase(@".#..#
+.....
+#####
+....#
+...##", 3, 4, 8)]
+        [TestCase(@"......#.#.
+#..#.#....
+..#######.
+.#.#.###..
+.#..#.....
+..#....#.#
+#..#....#.
+.##.#..###
+##...#..#.
+.#....####", 5, 8, 33)]
+        [TestCase(@"#.#...#.#.
+.###....#.
+.#....#...
+##.#.#.#.#
+....#.#.#.
+.##..###.#
+..#...##..
+..##....##
+......#...
+.####.###.", 1, 2, 35)]
+        [TestCase(@".#..#..###
+####.###.#
+....###.#.
+..###.##.#
+##.##.#.#.
+....###..#
+..#.#..#.#
+#..#.#.###
+.##...##.#
+.....#.#..", 6, 3, 41)]
+        [TestCase(@".#..##.###...#######
+##.############..##.
+.#.######.########.#
+.###.#######.####.#.
+#####.##.#.##.###.##
+..#####..#.#########
+####################
+#.####....###.#.#.##
+##.#################
+#####.##.###..####..
+..######..##.#######
+####.##.####...##..#
+.#####..#.######.###
+##...#.##########...
+#.##########.#######
+.####.#.###.###.#.##
+....##.##.###..#####
+.#.#.###########.###
+#.#.#.#####.####.###
+###.##.####.##.#..##", 11, 13, 210)]
+        public void PartATests(string input, int expX, int expY, int expDetected)
+        {
+            var day = new Day10();
+            var result = day.Solve(input);
+            Assert.AreEqual(expX, result.BestX);
+            Assert.AreEqual(expY, result.BestY);
+            Assert.AreEqual(expDetected, result.Detected);
+        }
+
+        [Test]
+        [TestCase("4, 5, 6", 456)]
+        public void PartBTests(string input, int expectedOutput)
+        {
+            var day = new Day10();
+            var result = day.SolveB(input);
+            Assert.AreEqual(expectedOutput, result);
+        }
+
+        [Test]
+        public void DoItA() // ?
+        {
+            var day = GetDayObject();
+            Console.WriteLine(day.SolveA(inputData));
+        }
+
+        [Test]
+        public void DoItB() // ?
+        {
+            var day = GetDayObject();
+            Console.WriteLine(day.SolveB(inputData));
+        }
+
+        [Test]
+        public void DoItA_Answer()
+        {
+            var day = GetDayObject();
+            var result = day.SolveA(inputData);
+            Assert.AreEqual(knownOutputA, result.ToString());
+        }
+
+        [Test]
+        public void DoItB_Answer()
+        {
+            var day = GetDayObject();
+            var result = day.SolveB(inputData);
+            Assert.AreEqual(knownOutputB, result.ToString());
+        }
+    }
+}
