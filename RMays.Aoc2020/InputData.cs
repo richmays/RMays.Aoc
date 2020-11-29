c0750544b3fca63884657c20a952a56bb4a5ca84
Rich Mays <richmays@gmail.com>
2020-11-29 00:39:38 -0500

    First commit for AoC 2020; preparation for D1

---
+++ RMays.Aoc2020/InputData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMays.Aoc2020
+{
+    public class InputData
+    {
+        #region Day0
+        public const string Day0 = @"";
+        #endregion
+
+    }
+}
