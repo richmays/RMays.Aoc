13cf37d21f12e80cdbf21367e91a6e43c1c21dcf
Rich Mays <richmays@gmail.com>
2020-12-02 00:42:30 -0500

    D2 done

---
+++ RMays.Aoc2020.Tests/Day2Tests.cs
@@ -0,0 +1,78 @@
+﻿using NUnit.Framework;
+using RMays.Aoc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMays.Aoc2020.Tests
+{
+    [TestFixture]
+    public class Day2Tests
+    {
+        // Official final times (after a delay):
+        // part 1:   6m13.73s
+        // part 1+2: 9m36.99s
+        private string inputData = InputData.Day2;
+        private string knownOutputA = "640";
+        private string knownOutputB = "472";
+
+        private IDay<long> GetDayObject()
+        {
+            return new Day2();
+        }
+
+        [Test]
+        [TestCase(@"1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc", 2)]
+        public void PartATests(string input, long expectedOutput)
+        {
+            var day = GetDayObject();
+            var result = day.Solve(input);
+            Assert.AreEqual(expectedOutput, result);
+        }
+
+        [Test]
+        [TestCase(@"1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc", 1)]
+        public void PartBTests(string input, long expectedOutput)
+        {
+            var day = GetDayObject();
+            var result = day.Solve(input, true);
+            Assert.AreEqual(expectedOutput, result);
+        }
+
+        [Test]
+        public void DoItA() // ?
+        {
+            var day = GetDayObject();
+            Console.WriteLine(day.Solve(inputData));
+        }
+
+        [Test]
+        public void DoItB() // ?
+        {
+            var day = GetDayObject();
+            Console.WriteLine(day.Solve(inputData, true));
+        }
+
+        [Test]
+        public void DoItA_Answer()
+        {
+            var day = GetDayObject();
+            var result = day.Solve(inputData);
+            Assert.AreEqual(knownOutputA, result.ToString());
+        }
+
+        [Test]
+        public void DoItB_Answer()
+        {
+            var day = GetDayObject();
+            var result = day.Solve(inputData, true);
+            Assert.AreEqual(knownOutputB, result.ToString());
+        }
+    }
+}
---
+++ RMays.Aoc2020/Day2.cs
@@ -0,0 +1,83 @@
+﻿using RMays.Aoc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMays.Aoc2020
+{
+    #region Day 2
+    /*
+--- Day 0: Template ---
+
+    */
+    #endregion
+
+    public class Day2 : IDay<long>
+    {
+        public long Solve(string input, bool IsPartB = false)
+        {
+            List<string> lines = Parser.TokenizeLines(input);
+            var valids = 0;
+            foreach(var line in lines)
+            {
+                if (IsPartB)
+                {
+                    if (IsValidB(line))
+                    {
+                        valids++;
+                    }
+                }
+                else
+                {
+
+
+                    if (IsValid(line))
+                    {
+                        valids++;
+                    }
+                }
+            }
+
+            return valids;
+
+
+
+        }
+
+        private bool IsValid(string line)
+        {
+            var tokens = line.Split(' ');
+            var min = int.Parse(tokens[0].Split('-')[0]);
+            var max = int.Parse(tokens[0].Split('-')[1]);
+            var ch = tokens[1][0];
+            var pass = tokens[2];
+
+            var freq = 0;
+            for(int i = 0; i < pass.Length; i++)
+            {
+                if(pass[i] == ch)
+                {
+                    freq++;
+                }
+
+            }
+            return (min <= freq && freq <= max);
+        }
+
+        private bool IsValidB(string line)
+        {
+            var tokens = line.Split(' ');
+            var first = int.Parse(tokens[0].Split('-')[0]) - 1;
+            var last = int.Parse(tokens[0].Split('-')[1]) - 1;
+            var ch = tokens[1][0];
+            var pass = tokens[2];
+
+            var v1 = pass[first] == ch;
+            var v2 = pass[last] == ch;
+            return v1 != v2;
+        }
+
+    }
+}
