91863a4b82a2906737945491f2a340f3dda977b5
Rich Mays <richmays@gmail.com>
2021-01-28 20:53:46 -0500

    D17 start; not solved yet

---
+++ RMays.Aoc2020/Day17.cs
@@ -0,0 +1,30 @@
+﻿using RMays.Aoc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMays.Aoc2020
+{
+    #region Day 0
+    /*
+--- Day 0: Template ---
+
+    */
+    #endregion
+
+    public class Day17 : IDay<long>
+    {
+        public long Solve(string input, bool IsPartB = false)
+        {
+            var lines = Parser.TokenizeLines(input);
+            foreach(var line in lines)
+            {
+
+            }
+
+            return lines.Count;
+        }
+    }
+}
