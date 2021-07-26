86c59fe8f9f31f9d573a0a2ea9c130986881a4c1
Rich Mays <richmays@gmail.com>
2021-07-26 17:59:15 -0400

    D19a done

---
+++ RMays.Aoc2020.Tests/Day19Tests.cs
@@ -0,0 +1,86 @@
+﻿using NUnit.Framework;
+using RMays.Aoc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMays.Aoc2020.Tests
+{
+    [TestFixture]
+    public class Day19Tests
+    {
+        // Final times:
+        // part 1:   XXmXX.XXs
+        // part 1+2: XXmXX.XXs
+
+        private string inputData = InputData.Day19;
+        private string knownOutputA = "123";
+        private string knownOutputB = "456";
+
+        private IDay<long> GetDayObject()
+        {
+            return new Day19();
+        }
+
+        [Test]
+        [TestCase(@"0: 4 1 5
+1: 2 3 | 3 2
+2: 4 4 | 5 5
+3: 4 5 | 5 4
+4: ""a""
+5: ""b""
+
+ababbb
+bababa
+abbbab
+aaabbb
+aaaabbb", 2)]
+        public void PartATests(string input, long expectedOutput)
+        {
+            var day = GetDayObject();
+            var result = day.Solve(input);
+            Assert.AreEqual(expectedOutput, result);
+        }
+
+        [Test]
+        [TestCase(@"4, 5, 6", 456)]
+        public void PartBTests(string input, long expectedOutput)
+        {
+            var day = GetDayObject();
+            var result = day.Solve(input, true);
+            Assert.AreEqual(expectedOutput, result);
+        }
+
+        [Test]
+        public void DoItA() // ?
+        {
+            var day = GetDayObject();
+            Console.WriteLine(day.Solve(inputData));
+        }
+
+        [Test]
+        public void DoItB() // ?
+        {
+            var day = GetDayObject();
+            Console.WriteLine(day.Solve(inputData, true));
+        }
+
+        [Test]
+        public void DoItA_Answer()
+        {
+            var day = GetDayObject();
+            var result = day.Solve(inputData);
+            Assert.AreEqual(knownOutputA, result.ToString());
+        }
+
+        [Test]
+        public void DoItB_Answer()
+        {
+            var day = GetDayObject();
+            var result = day.Solve(inputData, true);
+            Assert.AreEqual(knownOutputB, result.ToString());
+        }
+    }
+}
---
+++ RMays.Aoc2020/Day19.cs
@@ -0,0 +1,166 @@
+﻿using RMays.Aoc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RMays.Aoc2020
+{
+    #region Day 19
+    /*
+--- Day 19: Template ---
+
+    */
+    #endregion
+
+    public class Day19 : IDay<long>
+    {
+        public long Solve(string input, bool IsPartB = false)
+        {
+            // Can we build up a regex of rules?  That might be too easy.  Let's try it anyway.
+            // Input has 2 sections.
+            var lines = Parser.TokenizeLines(input);
+            var inTopSection = true;
+
+            var rules = new List<Rule>();
+            var messages = new List<string>();
+            foreach(var line in lines)
+            {
+                //Console.WriteLine(line);
+
+                if (line[0] == 'a' || line[0] == 'b')
+                {
+                    // Found a message
+                    messages.Add(line);
+                }
+                else
+                {
+                    // Found a rule
+                    var rule = new Rule(line);
+                    rules.Add(rule);
+                    //Console.WriteLine(rule);
+                }
+            }
+
+            var matches = 0;
+            var pattern = GetPattern(rules);
+
+            foreach(var message in messages)
+            {
+                var match = Regex.Match(message, pattern);
+                if (match.Success)
+                {
+                    matches++;
+                }
+            }
+
+            return matches;
+        }
+
+        private string GetPattern(List<Rule> rules)
+        {
+            var pattern = "^[0]$";
+
+            //Console.WriteLine(pattern);
+            while (pattern.Contains('['))
+            {
+                var startRuleId = pattern.IndexOf('[');
+                var endRuleId = pattern.IndexOf(']', startRuleId);
+                var ruleId = int.Parse(pattern.Substring(startRuleId + 1, endRuleId - startRuleId - 1));
+
+                pattern = pattern.Replace($"[{ruleId}]", rules.First(x => x.RuleId == ruleId).InnerReplace());
+                //Console.WriteLine(pattern);
+            }
+
+            return pattern;
+            //return "^a((aa|bb)(ab|ba)|(ab|ba)(aa|bb))b$";
+        }
+
+        internal class Rule
+        {
+            public int RuleId { get; set; }
+            public string RuleRaw { get; set; }
+            public List<List<int>> Options { get; set; }
+            public char? Leaf { get; set; }
+
+            public Rule(string lineRule)
+            {
+                RuleId = int.Parse(lineRule.Substring(0, lineRule.IndexOf(':')));
+                RuleRaw = lineRule.Substring(lineRule.IndexOf(':') + 2);
+                Options = new List<List<int>>();
+                Leaf = null;
+                if (RuleRaw[0] == '"')
+                {
+                    Leaf = RuleRaw[1];
+                    return;
+                }
+                var options = RuleRaw.Split(' ');
+                var currOption = new List<int>();
+                foreach(var token in options)
+                {
+                    if (string.IsNullOrWhiteSpace(token.ToString())) continue;
+                    if (token == "|")
+                    {
+                        Options.Add(currOption);
+                        currOption = new List<int>();
+                    }
+                    else
+                    {
+                        currOption.Add(int.Parse(token));
+                    }
+                }
+
+                if (currOption.Count() > 0)
+                {
+                    Options.Add(currOption);
+                }
+            }
+
+            public string InnerReplace()
+            {
+                var result = "";
+                foreach (var option in this.Options)
+                {
+                    result += "|";
+                    foreach(var suboption in option)
+                    {
+                        result += $"[{suboption}]";
+                    }
+                }
+                if (result.Length > 0)
+                {
+                    result = "(" + result.Substring(1) + ")";
+                }
+
+                if (Leaf != null)
+                {
+                    result = $"{Leaf}";
+                }
+
+                return result;
+            }
+
+            public override string ToString()
+            {
+                var options = "";
+                foreach (var option in this.Options)
+                {
+                    options += " | " + string.Join(" ", option);
+                }
+                if (options.Length > 1)
+                {
+                    options = options.Substring(3);
+                }
+
+                if (Leaf != null)
+                {
+                    options = $"\"{Leaf}\"";
+                }
+
+                return $"{this.RuleId}: {options}";
+            }
+        }
+    }
+}
