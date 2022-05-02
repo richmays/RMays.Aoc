566bb478c4d4729a96a71f183eea1b88fd774a03
Rich Mays <richmays@gmail.com>
2022-05-02 01:16:48 -0400

    D13 done

---
+++ RMays.Aoc2021.Tests/Day13Tests.cs
@@ -0,0 +1,115 @@
+﻿using NUnit.Framework;
+using RMays.Aoc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMays.Aoc2021.Tests
+{
+    [TestFixture]
+    public class Day13Tests
+    {
+        // Final times:
+        // part 1:   XXmXX.XXs
+        // part 1+2: XXmXX.XXs
+
+        private string inputData = InputData.Day13;
+        private string knownOutputA = "631";
+        private string knownOutputB = "92"; // EFLFJGRF
+
+        private IDay<long> GetDayObject()
+        {
+            return new Day13();
+        }
+
+        [Test]
+        [TestCase(@"6,10
+0,14
+9,10
+0,3
+10,4
+4,11
+6,0
+6,12
+4,1
+0,13
+10,12
+3,4
+3,0
+8,4
+1,10
+2,14
+8,10
+9,0
+
+fold along y=7
+fold along x=5", 17)]
+        public void PartATests(string input, long expectedOutput)
+        {
+            var day = GetDayObject();
+            var result = day.Solve(input);
+            Assert.AreEqual(expectedOutput, result);
+        }
+
+        [Test]
+        [TestCase(@"6,10
+0,14
+9,10
+0,3
+10,4
+4,11
+6,0
+6,12
+4,1
+0,13
+10,12
+3,4
+3,0
+8,4
+1,10
+2,14
+8,10
+9,0
+
+fold along y=7
+fold along x=5", 16)]
+        public void PartBTests(string input, long expectedOutput)
+        {
+            var day = GetDayObject();
+            var result = day.Solve(input, true);
+            Assert.AreEqual(expectedOutput, result);
+        }
+
+        [Test]
+        public void DoItA() // ?
+        {
+            var day = GetDayObject();
+            Console.WriteLine(day.Solve(inputData));
+        }
+
+        [Test]
+        public void DoItB() // ?
+        {
+            var day = GetDayObject();
+            Console.WriteLine(day.Solve(inputData, true));
+        }
+
+        [Test]
+        public void DoItA_Answer()
+        {
+            var day = GetDayObject();
+            var result = day.Solve(inputData);
+            Assert.AreEqual(knownOutputA, result.ToString());
+        }
+
+        [Test]
+        public void DoItB_Answer()
+        {
+            var day = GetDayObject();
+            var result = day.Solve(inputData, true);
+            Assert.AreEqual(knownOutputB, result.ToString());
+        }
+    }
+}
---
+++ RMays.Aoc2021/Day13.cs
@@ -0,0 +1,90 @@
+﻿using RMays.Aoc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMays.Aoc2021
+{
+    #region Day 13
+    /*
+--- Day 0: Template ---
+
+    */
+    #endregion
+
+    public class Day13 : IDay<long>
+    {
+        public long Solve(string input, bool IsPartB = false)
+        {
+            // Structure: List of coords.
+            var coords = new List<(int, int)>();
+            var lines = Parser.TokenizeLines(input);
+            foreach(var line in lines)
+            {
+                if (line.StartsWith("fold")) break;
+                var lineItems = line.Split(',');
+                coords.Add((int.Parse(lineItems[0]), int.Parse(lineItems[1])));
+            }
+
+            // Make folds.
+            lines = lines.Where(x => x.StartsWith("fold")).ToList();
+            foreach(var line in lines)
+            {
+                var axis = line.Split(' ')[2].Split('=')[0];
+                var foldIndex = int.Parse(line.Split(' ')[2].Split('=')[1]);
+                var newCoords = new List<(int, int)>();
+
+                // Fold!
+                foreach (var coord in coords)
+                {
+                    (int, int) newCoord = (0, 0);
+                    if (axis == "y")
+                    {
+                        newCoord = (coord.Item1, coord.Item2 <= foldIndex ? coord.Item2 : 2 * foldIndex - coord.Item2);
+                    }
+                    else if (axis == "x")
+                    {
+                        newCoord = (coord.Item1 <= foldIndex ? coord.Item1 : 2 * foldIndex - coord.Item1, coord.Item2);
+                    }
+                    else
+                    {
+                        throw new ApplicationException("Invalid axis: " + axis);
+                    }
+
+                    if (!newCoords.Contains(newCoord))
+                    {
+                        newCoords.Add(newCoord);
+                    }
+                }
+
+                coords = newCoords;
+
+                if (!IsPartB)
+                {
+                    // We're just folding once, so jump out.
+                    return coords.Count;
+                }
+            }
+
+            PrintBoard(coords);
+            return coords.Count;
+        }
+
+        private void PrintBoard(List<(int, int)> coords)
+        {
+            int maxX = coords.Max(x => x.Item1);
+            int maxY = coords.Max(x => x.Item2);
+
+            for(int y = 0; y <= maxY; y++)
+            {
+                for(int x = 0; x <= maxX; x++)
+                {
+                    Console.Write(coords.Contains((x, y)) ? 'X' : '.');
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
