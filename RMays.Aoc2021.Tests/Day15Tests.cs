c0b83fab814c2dd7d79efddcf97ce96178675ca0
Rich Mays <richmays@gmail.com>
2022-05-09 04:25:30 -0400

    D15 done, but slow.  Improve!

---
+++ RMays.Aoc2021.Tests/Day15Tests.cs
@@ -0,0 +1,103 @@
+﻿using NUnit.Framework;
+using RMays.Aoc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMays.Aoc2021.Tests
+{
+    [TestFixture]
+    public class Day15Tests
+    {
+        // Final times:
+        // part 1:   XXmXX.XXs
+        // part 1+2: XXmXX.XXs
+
+        private string inputData = InputData.Day15;
+        private string knownOutputA = "811";
+        private string knownOutputB = "3012"; // 21.9s to find; let's not run the whole grid.  Let's go diagonally.
+
+        private IDay<long> GetDayObject()
+        {
+            return new Day15();
+        }
+
+        [Test]
+        [TestCase(@"1163751742
+1381373672
+2136511328
+3694931569
+7463417111
+1319128137
+1359912421
+3125421639
+1293138521
+2311944581", 40)]
+        [TestCase(@"1999999999
+1999999999
+1911199999
+1119199999
+9999199999
+9999199999
+9999199999
+9999111199
+9999999119
+9999999911", 20)]
+        public void PartATests(string input, long expectedOutput)
+        {
+            var day = GetDayObject();
+            var result = day.Solve(input);
+            Assert.AreEqual(expectedOutput, result);
+        }
+
+        [Test]
+        [TestCase(@"1163751742
+1381373672
+2136511328
+3694931569
+7463417111
+1319128137
+1359912421
+3125421639
+1293138521
+2311944581", 315)]
+        public void PartBTests(string input, long expectedOutput)
+        {
+            var day = GetDayObject();
+            var result = day.Solve(input, true);
+            Assert.AreEqual(expectedOutput, result);
+        }
+
+        [Test]
+        public void DoItA() // ?
+        {
+            var day = GetDayObject();
+            Console.WriteLine(day.Solve(inputData));
+        }
+
+        [Test]
+        public void DoItB() // ?
+        {
+            var day = GetDayObject();
+            Console.WriteLine(day.Solve(inputData, true));
+        }
+
+        [Test]
+        public void DoItA_Answer()
+        {
+            var day = GetDayObject();
+            var result = day.Solve(inputData);
+            Assert.AreEqual(knownOutputA, result.ToString());
+        }
+
+        [Test]
+        public void DoItB_Answer()
+        {
+            var day = GetDayObject();
+            var result = day.Solve(inputData, true);
+            Assert.AreEqual(knownOutputB, result.ToString());
+        }
+    }
+}
