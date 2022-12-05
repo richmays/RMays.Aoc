f62c858764e0caa7335cc83f42a2d2842c014439
Rich Mays <richmays@gmail.com>
2022-12-05 00:38:57 -0500

    D5 done; tricky!  All about stacks.

---
+++ RMays.Aoc2022.Tests/Day5Tests.cs
@@ -0,0 +1,91 @@
+﻿using NUnit.Framework;
+using RMays.Aoc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMays.Aoc2022.Tests
+{
+    [TestFixture]
+    public class Day5Tests
+    {
+        // Final times:
+        // part 1:   XXmXX.XXs
+        // part 1+2: XXmXX.XXs
+
+        private string inputData = InputData.Day5;
+        private string knownOutputA = "FRDSQRRCD";
+        private string knownOutputB = "HRFTQVWNN";
+
+        private Day5 GetDayObject()
+        {
+            return new Day5();
+        }
+
+        [Test]
+        [TestCase(@"    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2", "CMZ")]
+        public void PartATests(string input, string expectedOutput)
+        {
+            var day = GetDayObject();
+            var result = day.Solve(input);
+            Assert.AreEqual(expectedOutput, result);
+        }
+
+        [Test]
+        [TestCase(@"    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2", "MCD")]
+        public void PartBTests(string input, string expectedOutput)
+        {
+            var day = GetDayObject();
+            var result = day.Solve(input, true);
+            Assert.AreEqual(expectedOutput, result);
+        }
+
+        [Test]
+        public void DoItA() // ?
+        {
+            var day = GetDayObject();
+            Console.WriteLine(day.Solve(inputData));
+        }
+
+        [Test]
+        public void DoItB() // ?
+        {
+            var day = GetDayObject();
+            Console.WriteLine(day.Solve(inputData, true));
+        }
+
+        [Test]
+        public void DoItA_Answer()
+        {
+            var day = GetDayObject();
+            var result = day.Solve(inputData);
+            Assert.AreEqual(knownOutputA, result.ToString());
+        }
+
+        [Test]
+        public void DoItB_Answer()
+        {
+            var day = GetDayObject();
+            var result = day.Solve(inputData, true);
+            Assert.AreEqual(knownOutputB, result.ToString());
+        }
+    }
+}
---
+++ RMays.Aoc2022/Day5.cs
@@ -0,0 +1,160 @@
+﻿using RMays.Aoc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMays.Aoc2022
+{
+    #region Day 0
+    /*
+--- Day 0: Template ---
+
+    */
+    #endregion
+
+    public class Day5
+    {
+        public string Solve(string input, bool IsPartB = false)
+        {
+            var wasReversed = false;
+            var lines = Parser.TokenizeLinesKeepSpaces(input);
+            var stacks = new List<Stack<char>>();
+            for (int i = 0; i < 10; i++)
+            {
+                stacks.Add(new Stack<char>());
+            }
+
+            foreach(var line in lines)
+            {
+                if (line.Length == 0)
+                {
+                    // Reverse everything!
+                    foreach (var stack in stacks)
+                    {
+                        ReverseStack(stack);
+                    }
+                }
+                else if (line[0] == 'm')
+                {
+                    if (!IsPartB)
+                    {
+                        HandleMove(stacks, line);
+                    }
+                    else
+                    {
+                        HandleMoveB(stacks, line);
+                    }
+                    continue;
+                }
+                else
+                {
+                    var currStack = 1;
+                    for (int i = 1; i < line.Length; i += 4)
+                    {
+                        var c = line[i];
+                        if (c != ' ')
+                        {
+                            stacks[currStack].Push(c);
+                        }
+                        //Console.Write(c);
+                        currStack++;
+                    }
+                    //Console.WriteLine();
+                }
+            }
+
+            PrintStacks(stacks);
+
+            var result = "";
+            foreach(var stack in stacks)
+            {
+                if (stack.Any())
+                {
+                    result += stack.Peek();
+                }
+            }
+            return result;
+        }
+
+        private void PrintStacks(List<Stack<char>> stacks)
+        {
+            return;
+            for (var stackId = 0; stackId < 10; stackId++)
+            {
+                if (!stacks[stackId].Any())
+                {
+                    continue;
+                }
+                Console.WriteLine($"Stack {stackId}:");
+                var tmpStack = new Stack<char>();
+                var currStack = stacks[stackId];
+                while (currStack.Any())
+                {
+                    var item = currStack.Pop();
+                    Console.WriteLine($" {item}");
+                    tmpStack.Push(item);
+                }
+
+                while (tmpStack.Any())
+                {
+                    currStack.Push(tmpStack.Pop());
+                }
+            }
+        }
+
+        private void HandleMove(List<Stack<char>> stacks, string line)
+        {
+            var quantity = int.Parse(line.Split(' ')[1]);
+            var source = int.Parse(line.Split(' ')[3]);
+            var dest = int.Parse(line.Split(' ')[5]);
+            //Console.WriteLine($"Moving: {quantity} boxes from {source} to {dest}.");
+
+            for (int moves = 0; moves < quantity; moves++)
+            {
+                //Console.WriteLine($"Moving box {stacks[source].Peek()} from {source} to {dest}.");
+                stacks[dest].Push(stacks[source].Pop());
+            }
+        }
+        private void HandleMoveB(List<Stack<char>> stacks, string line)
+        {
+            var quantity = int.Parse(line.Split(' ')[1]);
+            var source = int.Parse(line.Split(' ')[3]);
+            var dest = int.Parse(line.Split(' ')[5]);
+            //Console.WriteLine($"Moving: {quantity} boxes from {source} to {dest}.");
+
+            var tmpStack = new Stack<char>();
+            for (int moves = 0; moves < quantity; moves++)
+            {
+                tmpStack.Push(stacks[source].Pop());
+            }
+
+            while(tmpStack.Any())
+            {
+                stacks[dest].Push(tmpStack.Pop());
+            }
+        }
+
+        private void ReverseStack(Stack<char> stack)
+        {
+            var tmpStack = new Stack<char>();
+            while(stack.Any())
+            {
+                tmpStack.Push(stack.Pop());
+            }
+
+            var tmpStack2 = new Stack<char>();
+            while (tmpStack.Any())
+            {
+                tmpStack2.Push(tmpStack.Pop());
+            }
+
+            while (tmpStack2.Any())
+            {
+                stack.Push(tmpStack2.Pop());
+            }
+        }
+
+    }
+}
