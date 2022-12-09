aa6eaa5b10428749c524deba59b2f70bb060363b
Rich Mays <richmays@gmail.com>
2022-12-08 22:13:34 -0500

    D8 done

---
+++ RMays.Aoc2022.Tests/Day8Tests.cs
@@ -0,0 +1,83 @@
+﻿using NUnit.Framework;
+using RMays.Aoc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMays.Aoc2022.Tests
+{
+    [TestFixture]
+    public class Day8Tests
+    {
+        // Final times:
+        // part 1:   XXmXX.XXs
+        // part 1+2: XXmXX.XXs
+
+        private string inputData = InputData.Day8;
+        private string knownOutputA = "1851";
+        private string knownOutputB = "574080";
+
+        private IDay<long> GetDayObject()
+        {
+            return new Day8();
+        }
+
+        [Test]
+        [TestCase(@"30373
+25512
+65332
+33549
+35390", 21)]
+        public void PartATests(string input, long expectedOutput)
+        {
+            var day = GetDayObject();
+            var result = day.Solve(input);
+            Assert.AreEqual(expectedOutput, result);
+        }
+
+        [Test]
+        [TestCase(@"30373
+25512
+65332
+33549
+35390", 8)]
+        public void PartBTests(string input, long expectedOutput)
+        {
+            var day = GetDayObject();
+            var result = day.Solve(input, true);
+            Assert.AreEqual(expectedOutput, result);
+        }
+
+        [Test]
+        public void DoItA() // ?
+        {
+            var day = GetDayObject();
+            Console.WriteLine(day.Solve(inputData));
+        }
+
+        [Test]
+        public void DoItB() // ?
+        {
+            var day = GetDayObject();
+            Console.WriteLine(day.Solve(inputData, true));
+        }
+
+        [Test]
+        public void DoItA_Answer()
+        {
+            var day = GetDayObject();
+            var result = day.Solve(inputData);
+            Assert.AreEqual(knownOutputA, result.ToString());
+        }
+
+        [Test]
+        public void DoItB_Answer()
+        {
+            var day = GetDayObject();
+            var result = day.Solve(inputData, true);
+            Assert.AreEqual(knownOutputB, result.ToString());
+        }
+    }
+}
---
+++ RMays.Aoc2022/Day8.cs
@@ -0,0 +1,245 @@
+﻿using RMays.Aoc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMays.Aoc2022
+{
+    #region Day 0
+    /*
+--- Day 0: Template ---
+
+    */
+    #endregion
+
+    public class Day8 : IDay<long>
+    {
+        private int Rows { get; set; }
+        private int Cols { get; set; }
+        private string Data { get; set; }
+
+        private HashSet<int> VisibleTrees { get; set; }
+
+        public long Solve(string input, bool IsPartB = false)
+        {
+            VisibleTrees = new HashSet<int>();
+            Cols = Parser.TokenizeLines(input)[0].Length;
+            Data = input.Replace("\r\n", "");
+            Rows = Data.Length / Cols;
+
+            if (IsPartB) return SolveB();
+
+            // Works!
+            //Console.WriteLine($"Height: {height}, width: {width}");
+
+            for (int r = 0; r < Rows; r++)
+            {
+                int lowest = -1;
+                for (int c = 0; c < Cols; c++)
+                {
+                    var cell = GetCell(r, c);
+                    if (cell > lowest)
+                    {
+                        lowest = cell;
+                        AddToSet(r, c);
+                    }
+                }
+
+                lowest = -1;
+                for (int c = Cols - 1; c >= 0; c--)
+                {
+                    var cell = GetCell(r, c);
+                    if (cell > lowest)
+                    {
+                        lowest = cell;
+                        AddToSet(r, c);
+                    }
+                }
+            }
+
+            for (int r = Rows - 1; r >= 0; r--)
+            {
+                int lowest = -1;
+                for (int c = 0; c < Cols; c++)
+                {
+                    var cell = GetCell(r, c);
+                    if (cell > lowest)
+                    {
+                        lowest = cell;
+                        AddToSet(r, c);
+                    }
+                }
+
+                lowest = -1;
+                for (int c = Cols - 1; c >= 0; c--)
+                {
+                    var cell = GetCell(r, c);
+                    if (cell > lowest)
+                    {
+                        lowest = cell;
+                        AddToSet(r, c);
+                    }
+                }
+            }
+
+            for (int c = 0; c < Cols; c++)
+            {
+                int lowest = -1;
+                for (int r = 0; r < Rows; r++)
+                {
+                    var cell = GetCell(r, c);
+                    if (cell > lowest)
+                    {
+                        lowest = cell;
+                        AddToSet(r, c);
+                    }
+                }
+
+                lowest = -1;
+                for (int r = Rows - 1; r >= 0; r--)
+                {
+                    var cell = GetCell(r, c);
+                    if (cell > lowest)
+                    {
+                        lowest = cell;
+                        AddToSet(r, c);
+                    }
+                }
+            }
+
+            for (int c = Cols - 1; c >= 0; c--)
+            {
+                int lowest = -1;
+                for (int r = 0; r < Rows; r++)
+                {
+                    var cell = GetCell(r, c);
+                    if (cell > lowest)
+                    {
+                        lowest = cell;
+                        AddToSet(r, c);
+                    }
+                }
+
+                lowest = -1;
+                for (int r = Rows - 1; r >= 0; r--)
+                {
+                    var cell = GetCell(r, c);
+                    if (cell > lowest)
+                    {
+                        lowest = cell;
+                        AddToSet(r, c);
+                    }
+                }
+            }
+
+            return VisibleTrees.Count();
+        }
+
+        private long SolveB()
+        {
+            //var getSS = GetScenicScore(3, 2);
+            //return -1;
+
+
+
+            long bestScore = -1;
+            for (int r = 0; r < Rows; r++)
+            {
+                for (int c = 0; c < Cols; c++)
+                {
+                    var score = GetScenicScore(r, c);
+                    if (score > bestScore)
+                    {
+                        bestScore = score;
+                    }
+                }
+            }
+
+            return bestScore;
+        }
+
+        private long GetScenicScore(int row, int col)
+        {
+            var score = 1;
+            int visibility;
+            var treeHeight = GetCell(row, col);
+
+            visibility = 0;
+            for (int r = row - 1; r >= 0; r--)
+            {
+                if (r < 0) break;
+                var thisTreeHeight = GetCell(r, col);
+                visibility++;
+                if (thisTreeHeight >= treeHeight)
+                {
+                    break;
+                }
+            }
+            //Console.WriteLine($"Up: {visibility}");
+            score *= visibility;
+
+            visibility = 0;
+            for (int r = row + 1; r < Rows; r++)
+            {
+                if (r >= Rows) break;
+                var thisTreeHeight = GetCell(r, col);
+                visibility++;
+                if (thisTreeHeight >= treeHeight)
+                {
+                    break;
+                }
+
+            }
+            //Console.WriteLine($"Down: {visibility}");
+            score *= visibility;
+
+            visibility = 0;
+            for (int c = col - 1; c >= 0; c--)
+            {
+                if (c < 0) break;
+                var thisTreeHeight = GetCell(row, c);
+                visibility++;
+                if (thisTreeHeight >= treeHeight)
+                {
+                    break;
+                }
+
+            }
+            //Console.WriteLine($"Left: {visibility}");
+            score *= visibility;
+
+            visibility = 0;
+            for (int c = col + 1; c < Cols; c++)
+            {
+                if (c >= Cols) break;
+                var thisTreeHeight = GetCell(row, c);
+                visibility++;
+                if (thisTreeHeight >= treeHeight)
+                {
+                    break;
+                }
+
+            }
+            //Console.WriteLine($"Right: {visibility}");
+            score *= visibility;
+
+            return score;
+        }
+
+        private int GetCell(int r, int c)
+        {
+            return Data[Rows * r + c] - '0';
+        }
+
+        private void AddToSet(int r, int c)
+        {
+            var spot = Rows * r + c;
+            if (!VisibleTrees.Contains(spot))
+            {
+                VisibleTrees.Add(spot);
+            }
+        }
+    }
+}
