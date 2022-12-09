7bb012a65b3beac2b7c9ab338c05042198a83860
Rich Mays <richmays@gmail.com>
2022-12-09 01:38:28 -0500

    D9 done.  Updated D8 input.

---
+++ RMays.Aoc2022.Tests/Day9Tests.cs
@@ -0,0 +1,97 @@
+﻿using NUnit.Framework;
+using RMays.Aoc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMays.Aoc2022.Tests
+{
+    [TestFixture]
+    public class Day9Tests
+    {
+        // Final times:
+        // part 1:   XXmXX.XXs
+        // part 1+2: XXmXX.XXs
+
+        private string inputData = InputData.Day9;
+        private string knownOutputA = "6011";
+        private string knownOutputB = "2419";
+
+        private IDay<long> GetDayObject()
+        {
+            return new Day9();
+        }
+
+        [Test]
+        [TestCase(@"R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2", 13)]
+        public void PartATests(string input, long expectedOutput)
+        {
+            var day = GetDayObject();
+            var result = day.Solve(input);
+            Assert.AreEqual(expectedOutput, result);
+        }
+
+        [Test]
+        [TestCase(@"R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2", 1)]
+        [TestCase(@"R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20", 36)]
+        public void PartBTests(string input, long expectedOutput)
+        {
+            var day = GetDayObject();
+            var result = day.Solve(input, true);
+            Assert.AreEqual(expectedOutput, result);
+        }
+
+        [Test]
+        public void DoItA() // ?
+        {
+            var day = GetDayObject();
+            Console.WriteLine(day.Solve(inputData));
+        }
+
+        [Test]
+        public void DoItB() // ?
+        {
+            var day = GetDayObject();
+            Console.WriteLine(day.Solve(inputData, true));
+        }
+
+        [Test]
+        public void DoItA_Answer()
+        {
+            var day = GetDayObject();
+            var result = day.Solve(inputData);
+            Assert.AreEqual(knownOutputA, result.ToString());
+        }
+
+        [Test]
+        public void DoItB_Answer()
+        {
+            var day = GetDayObject();
+            var result = day.Solve(inputData, true);
+            Assert.AreEqual(knownOutputB, result.ToString());
+        }
+    }
+}
---
+++ RMays.Aoc2022/Day9.cs
@@ -0,0 +1,142 @@
+﻿using RMays.Aoc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMays.Aoc2022
+{
+    #region Day 0
+    /*
+--- Day 0: Template ---
+
+    */
+    #endregion
+
+    public class Day9 : IDay<long>
+    {
+        HashSet<(int, int)> SpotsVisited;
+        List<(int, int)> Rope = new List<(int, int)>();
+
+        public long Solve(string input, bool IsPartB = false)
+        {
+            int ropeLength = (IsPartB ? 10 : 2);
+            for (int i = 0; i < ropeLength; i++)
+            {
+                Rope.Add((0, 0));
+            }
+
+            SpotsVisited = new HashSet<(int, int)>();
+            var lines = Parser.TokenizeLines(input);
+
+            foreach (var line in lines)
+            {
+                var dir = line[0];
+                var steps = int.Parse(line.Split(' ')[1]);
+                switch (dir)
+                {
+                    case 'U':
+                        for (int i = 0; i < steps; i++)
+                        {
+                            Rope[0] = (Rope[0].Item1 - 1, Rope[0].Item2);
+                            UpdateTail();
+                        }
+                        break;
+                    case 'D':
+                        for (int i = 0; i < steps; i++)
+                        {
+                            Rope[0] = (Rope[0].Item1 + 1, Rope[0].Item2);
+                            UpdateTail();
+                        }
+                        break;
+                    case 'L':
+                        for (int i = 0; i < steps; i++)
+                        {
+                            Rope[0] = (Rope[0].Item1, Rope[0].Item2 - 1);
+                            UpdateTail();
+                        }
+                        break;
+                    case 'R':
+                        for (int i = 0; i < steps; i++)
+                        {
+                            Rope[0] = (Rope[0].Item1, Rope[0].Item2 + 1);
+                            UpdateTail();
+                        }
+                        break;
+                }
+            }
+
+            return SpotsVisited.Count();
+        }
+
+        public void UpdateTail()
+        {
+            for (int i = 1; i < Rope.Count(); i++)
+            {
+                var row = Rope[i - 1].Item1;
+                var col = Rope[i - 1].Item2;
+                var tRow = Rope[i].Item1;
+                var tCol = Rope[i].Item2;
+
+                if (tCol < col - 1)
+                {
+                    // Rope[i] = (Rope[i].Item1, Rope[i].Item2);
+                    Rope[i] = (Rope[i].Item1, Rope[i].Item2 + 1);
+                    if (tRow < row)
+                    {
+                        Rope[i] = (Rope[i].Item1 + 1, Rope[i].Item2);
+                    }
+                    else if (tRow > row)
+                    {
+                        Rope[i] = (Rope[i].Item1 - 1, Rope[i].Item2);
+                    }
+                }
+                else if (tCol > col + 1)
+                {
+                    Rope[i] = (Rope[i].Item1, Rope[i].Item2 - 1);
+                    if (tRow < row)
+                    {
+                        Rope[i] = (Rope[i].Item1 + 1, Rope[i].Item2);
+                    }
+                    else if (tRow > row)
+                    {
+                        Rope[i] = (Rope[i].Item1 - 1, Rope[i].Item2);
+                    }
+                }
+                else if (tRow < row - 1)
+                {
+                    Rope[i] = (Rope[i].Item1 + 1, Rope[i].Item2);
+                    if (tCol < col)
+                    {
+                        Rope[i] = (Rope[i].Item1, Rope[i].Item2 + 1);
+                    }
+                    else if (tCol > col)
+                    {
+                        Rope[i] = (Rope[i].Item1, Rope[i].Item2 - 1);
+                    }
+                }
+                else if (tRow > row + 1)
+                {
+                    Rope[i] = (Rope[i].Item1 - 1, Rope[i].Item2);
+                    if (tCol < col)
+                    {
+                        Rope[i] = (Rope[i].Item1, Rope[i].Item2 + 1);
+                    }
+                    else if (tCol > col)
+                    {
+                        Rope[i] = (Rope[i].Item1, Rope[i].Item2 - 1);
+                    }
+                }
+            }
+
+            //Console.WriteLine($"  T After: {tRow},{tCol}");
+
+            var lastRope = Rope.Last();
+            if (!SpotsVisited.Contains(lastRope))
+            {
+                SpotsVisited.Add(lastRope);
+            }
+        }
+    }
+}
