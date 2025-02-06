a27b934c7b5a09c134bd057dc9825cdaae824a34
Rich Mays <richmays@gmail.com>
2025-02-06 02:26:38 -0500

    D2 done

---
+++ RMays.Aoc2024.Tests/Day2Tests.cs
@@ -0,0 +1,85 @@
+﻿using NUnit.Framework;
+using RMays.Aoc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMays.Aoc2024.Tests
+{
+    [TestFixture]
+    public class Day2Tests
+    {
+        // Final times:
+        // part 1:   XXmXX.XXs
+        // part 1+2: XXmXX.XXs
+
+        private string inputData = InputData.Day2;
+        private string knownOutputA = "483";
+        private string knownOutputB = "528";
+
+        private IDay<long> GetDayObject()
+        {
+            return new Day2();
+        }
+
+        [Test]
+        [TestCase(@"7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9", 2)]
+        public void PartATests(string input, long expectedOutput)
+        {
+            var day = GetDayObject();
+            var result = day.Solve(input);
+            Assert.AreEqual(expectedOutput, result);
+        }
+
+        [Test]
+        [TestCase(@"7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9", 4)]
+        public void PartBTests(string input, long expectedOutput)
+        {
+            var day = GetDayObject();
+            var result = day.Solve(input, true);
+            Assert.AreEqual(expectedOutput, result);
+        }
+
+        [Test]
+        public void DoItA() // ?
+        {
+            var day = GetDayObject();
+            Console.WriteLine(day.Solve(inputData));
+        }
+
+        [Test]
+        public void DoItB() // ?
+        {
+            var day = GetDayObject();
+            Console.WriteLine(day.Solve(inputData, true));
+        }
+
+        [Test]
+        public void DoItA_Answer()
+        {
+            var day = GetDayObject();
+            var result = day.Solve(inputData);
+            Assert.AreEqual(knownOutputA, result.ToString());
+        }
+
+        [Test]
+        public void DoItB_Answer()
+        {
+            var day = GetDayObject();
+            var result = day.Solve(inputData, true);
+            Assert.AreEqual(knownOutputB, result.ToString());
+        }
+    }
+}
---
+++ RMays.Aoc2024/Day2.cs
@@ -0,0 +1,85 @@
+﻿using RMays.Aoc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMays.Aoc2024
+{
+    public class Day2 : IDay<long>
+    {
+        public long Solve(string input, bool IsPartB = false)
+        {
+            var safeReports = 0;
+            var lines = Parser.TokenizeLines(input);
+            foreach (var line in lines)
+            {
+                if (!IsPartB)
+                {
+                    if (!ReportHasFault(line.Split(' ').Select(x => int.Parse(x)).ToList()))
+                    {
+                        safeReports++;
+                    }
+                }
+                else
+                {
+                    // Brute force.  Ugh.
+                    var hasNoFaults = false;
+                    var originalList = line.Split(' ').Select(x => int.Parse(x)).ToList();
+                    for (int i = 0; i < originalList.Count; i++)
+                    {
+                        var newList = new List<int>();
+                        for (int j = 0; j < originalList.Count; j++)
+                        {
+                            if (i == j) continue;
+                            newList.Add(originalList[j]);
+                        }
+
+                        if (!ReportHasFault(newList))
+                        {
+                            hasNoFaults = true;
+                        }
+                    }
+
+                    if (hasNoFaults)
+                    {
+                        safeReports++;
+                    }
+
+                }
+            }
+
+            return safeReports;
+        }
+
+        private bool ReportHasFault(List<int> levels)
+        {
+            if (levels.Count <= 1) return false;
+            int prev = levels[0];
+            int direction = levels[0] < levels[1] ? 1 : -1;
+            for (int i = 1; i < levels.Count; i++)
+            {
+                if (Math.Abs(levels[i] - prev) > 3)
+                {
+                    return true;
+                }
+
+                if (levels[i] == prev)
+                {
+                    return true;
+                }
+
+                if ((levels[i] - prev) * direction < 0)
+                {
+                    return true;
+                }
+
+                prev = levels[i];
+            }
+
+            return false;
+        }
+    }
+}
