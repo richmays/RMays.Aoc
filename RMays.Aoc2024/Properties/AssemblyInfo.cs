eb3b88cb69eb678849eaf154a0733494eb8a0438
Rich Mays <richmays@gmail.com>
2025-02-08 00:33:32 -0500

    Updated AssemblyInfo and the csproj files.

--- RMays.Aoc2024/Properties/AssemblyInfo.cs
+++ RMays.Aoc2024/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RMays.Aoc2023")]
+[assembly: AssemblyTitle("RMays.Aoc2024")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RMays.Aoc2023")]
+[assembly: AssemblyProduct("RMays.Aoc2024")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
