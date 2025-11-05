ff1cd45cd905e3d3f05c373328a6076098590184
Rich Mays <richmays@gmail.com>
2025-11-05 09:01:59 -0500

    Day 4a done

---
+++ RMays.Aoc2024.Tests/Day4Tests.cs
@@ -0,0 +1,109 @@
+﻿using NUnit.Framework;
+using RMays.Aoc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMays.Aoc2024.Tests
+{
+    [TestFixture]
+    public class Day4Tests
+    {
+        // Final times:
+        // part 1:   XXmXX.XXs
+        // part 1+2: XXmXX.XXs
+
+        private string inputData = InputData.Day4;
+        private string knownOutputA = "2567";
+        private string knownOutputB = "456";
+
+        private IDay<long> GetDayObject()
+        {
+            return new Day4();
+        }
+
+        [Test]
+        [TestCase(@"MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX", "Default", 18)]
+        [TestCase(@"XMAS
+SAMX", "Horizontal", 2)]
+        [TestCase("XMASAMX", "Horizontal one line", 2)]
+        [TestCase("XMASXMAS", "Horizontal two on one line", 2)]
+        [TestCase(@"X
+M
+A
+S
+A
+M
+X", "Vertical", 2)]
+        [TestCase(@"X......
+.M.....
+..A....
+...S...
+....A..
+.....M.
+......X", "TopLeft to BottomRight", 2)]
+        [TestCase(@"......X
+.....M.
+....A..
+...S...
+..A....
+.M.....
+X......", "TopRight to BottomLeft", 2)]
+        public void PartATests(string input, string testcaseName, long expectedOutput)
+        {
+            var day = GetDayObject();
+            var result = day.Solve(input);
+            Assert.AreEqual(expectedOutput, result, $"Failed test: {testcaseName}");
+        }
+
+        [Test]
+        [TestCase(@"4, 5, 6", 456)]
+        public void PartBTests(string input, long expectedOutput)
+        {
+            var day = GetDayObject();
+            var result = day.Solve(input, true);
+            Assert.AreEqual(expectedOutput, result);
+        }
+
+        [Test]
+        public void DoItA() // ?
+        {
+            var day = GetDayObject();
+            Console.WriteLine(day.Solve(inputData));
+        }
+
+        [Test]
+        public void DoItB() // ?
+        {
+            var day = GetDayObject();
+            Console.WriteLine(day.Solve(inputData, true));
+        }
+
+        [Test]
+        public void DoItA_Answer()
+        {
+            var day = GetDayObject();
+            var result = day.Solve(inputData);
+            Assert.AreEqual(knownOutputA, result.ToString());
+        }
+
+        [Test]
+        public void DoItB_Answer()
+        {
+            var day = GetDayObject();
+            var result = day.Solve(inputData, true);
+            Assert.AreEqual(knownOutputB, result.ToString());
+        }
+    }
+}
---
+++ RMays.Aoc2024/Day4.cs
@@ -0,0 +1,130 @@
+﻿using RMays.Aoc;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMays.Aoc2024
+{
+    #region Day 0
+    /*
+--- Day 0: Template ---
+
+    */
+    #endregion
+
+    public class Day4 : IDay<long>
+    {
+        public long Solve(string input, bool IsPartB = false)
+        {
+            var sum = 0;
+            var lines = Parser.TokenizeLines(input);
+
+            // Horizontal
+            Log("Starting horizontal checks");
+            foreach (var line in lines)
+            {
+                sum += GetXmasesCount(line);
+                sum += GetXmasesCount(Reverse(line));
+            }
+
+            // Vertical
+            Log("Starting vertical checks");
+            sum += GetVerticalXmasesCount(lines);
+
+            // Diagonal:  \ and /
+            Log("Starting topleft to bottomright checks");
+            // Shift everything to the right X columns, where X is the row minus the total rows minus 1.
+            var newLinesTL = new List<string>();
+            // Shift everything to the left X columns, where X is the row minus the total rows minus 1.
+            var newLinesTR = new List<string>();
+            var r = 0;
+            foreach (var line in lines)
+            {
+                var newLineTL = new StringBuilder();
+                var newLineTR = new StringBuilder();
+                for (int c = lines.Count - r; c > 1; c--)
+                {
+                    newLineTL.Append(".");
+                }
+                for (int c = 0; c < r; c++)
+                {
+                    newLineTR.Append(".");
+                }
+                newLineTL.Append(line);
+                newLineTR.Append(line);
+                while (newLineTL.Length < lines.Count + lines[0].Length - 1)
+                {
+                    newLineTL.Append(".");
+                }
+                while (newLineTR.Length < lines.Count + lines[0].Length - 1)
+                {
+                    newLineTR.Append(".");
+                }
+                Log(newLineTL.ToString());
+                Log(newLineTR.ToString());
+                newLinesTL.Add(newLineTL.ToString());
+                newLinesTR.Add(newLineTR.ToString());
+
+                r++;
+            }
+            sum += GetVerticalXmasesCount(newLinesTL);
+            sum += GetVerticalXmasesCount(newLinesTR);
+
+            return sum;
+        }
+
+        private int GetVerticalXmasesCount(List<string> lines)
+        {
+            int sum = 0;
+            for (int c = 0; c < lines[0].Length; c++)
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (var line in lines)
+                {
+                    sb.Append(line[c]);
+                }
+
+                sum += GetXmasesCount(sb.ToString());
+                sum += GetXmasesCount(Reverse(sb.ToString()));
+            }
+            return sum;
+        }
+
+        private void Log(string log)
+        {
+            //Console.WriteLine(log);
+        }
+
+
+        private string Reverse(string source)
+        {
+            return new string(source.Reverse().ToArray());
+        }
+
+        private int GetXmasesCount(string input)
+        {
+            Log("Looking here: " + input);
+            int c = 0;
+            int found = 0;
+            while (c < input.Length)
+            {
+                var loc = input.IndexOf("XMAS", c);
+                if (loc >= 0)
+                {
+                    found++;
+                    c = loc + 4;
+                }
+                else
+                {
+                    // Long way to jump out
+                    c = input.Length + 1;
+                }
+            }
+            return found;
+        }
+    }
+}
